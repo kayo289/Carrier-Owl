624c9875afed928ed37428e52b786e0f47afba83
kayo <39555606+kayo289@users.noreply.github.com>
2021-06-12 15:54:46 +0900

    Update carrier_owl.py

--- src/carrier_owl.py
+++ src/carrier_owl.py
@@ -80,14 +80,11 @@ def notify(results: list, slack_id: str, line_token: str) -> None:
     # 通知
     star = '*'*80
     today = datetime.date.today()
+    n_articles = len(results)
     text = f'{star}\n \t \t {today}\tnum of articles = {n_articles}(多くても7件までしか表示しない)\n読むぞ〜〜〜〜〜〜〜〜〜〜〜〜〜！！！！！！！\n{star}'
     send2app(text, slack_id, line_token)
-    count = 1
     # descending
     for result in sorted(results, reverse=True, key=lambda x: x.score):
-        if count == 7:
-            break
-        count+=1
         url = result.url
         title = result.title
         abstract = result.abstract
